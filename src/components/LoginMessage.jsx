adfabbad3b4f2393a3beb235858dfd642ac63d69
Alyssa DiCarlo <alyssa.dicarlo@gmail.com>
2021-07-23 09:04:05 -0700

    initial commit

---
+++ src/components/LoginMessage.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function LoginMessage(props) {
+    const { message, isValid } = props;
+    
+    if (message) {
+        return (
+            <h1 className={isValid ? 'success' : 'error'}>{message}</h1>
+        );
+    } else {
+        return null;
+    }
+}
+
+export default LoginMessage;
